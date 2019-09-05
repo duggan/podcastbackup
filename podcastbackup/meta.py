4b3d57db18836ba7e34884b2d3a658ed4f5929e9
Ross Duggan <ross@duggan.ie>
2019-09-05 12:47:46 +0100

    0.1.2 -> 0.1.3

--- podcastbackup/meta.py
+++ podcastbackup/meta.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__         = "0.1.2"
+__version__         = "0.1.3"
 __desc__            = "A podcast backup helper."
 __author__          = "Ross Duggan"
 __author_email__    = "ross@duggan.ie"
